92218cb7fe14447739545904534ef1b0683a72a4
Adam Britton <adzbritton@btinternet.com>
2018-05-04 17:05:06 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CardList from'./CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+const state = {
+	robots: robots,
+	searchfield: '',
+}
+
+class App extends Component {
+	constructor () {
+		super()
+		this.state= {
+			robots: robots,
+			searchfield: '',
+		}
+	}
+
+	onSearchChange(event){
+		console.log(event);
+	}
+
+	render () {
+		return (
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+				<SearchBox SearchChange={this.onSearchChange}/>
+				<CardList robots={this.state.robots}/>
+			</div>		
+		);
+	}
+}
+
+
+export default App
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Adam Britton',
+		username: 'adz',
+		email: 'Me@adam.com'
+	},
+	{
+		id: 2,
+		name: 'Craig Nolan',
+		username: 'cjn',
+		email: 'Me@craig.com'
+	},
+	{
+		id: 3,
+		name: 'Ellen Beardsell',
+		username: 'beado',
+		email: 'Me@ellen.com'
+	},
+	{
+		id: 4,
+		name: 'Philip Cadden',
+		username: 'pip',
+		email: 'Me@philip.com'
+	},
+	{
+		id: 5,
+		name: 'Maggie Britton',
+		username: 'MJ',
+		email: 'Me@maggie.com'
+	},
+	{
+		id: 6,
+		name: 'Teddy Britton',
+		username: 'Ted',
+		email: 'Me@ted.com'
+	},
+	{
+		id: 7,
+		name: 'Fiona Nolan',
+		username: 'fi',
+		email: 'Me@fiona.com'
+	},
+	{
+		id: 8,
+		name: 'Adam Garvin',
+		username: 'garvin',
+		email: 'Me@garvin.com'
+	},
+	{
+		id: 9,
+		name: 'Lily Garvin',
+		username: 'Lills',
+		email: 'Me@lily.com'
+	},
+	{
+		id: 10,
+		name: 'Millie Nolan',
+		username: 'Mills',
+		email: 'Me@millie.com'
+	},
+]
